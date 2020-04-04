b3420010091f12d61217daf4c9691ce75ee3ddd2
José Manuel Bermudo <josemanuel.bermudodelgado@cognizant.com>
2020-04-04 13:22:16 +0200

    added simple flask server

--- api.py
+++ api.py
@@ -1,31 +1,45 @@
 import sys
+from flask import Flask, abort, jsonify, request
 from populartimes import populartimes 
-print("Popular times by your coordinates using Google Places API & populartimes crawler");
 
-# Coordinates of the user
-lat = 40.284645
-lon = -3.798970
+app = Flask(__name__)
 
-# API key
-apiKey = sys.argv[1]
+@app.route('/')
+def index():
+  return 'Server Works and running, use getPopularTimes/ service'
 
-distance = 0.0003
-threads = 20
-radius = 100
-allPlaces = False
-placeTypes = []
+@app.route('/getPopularTimes')
+def getPopularTimes():
+  apiKey = request.args.get('apiKey', type = str)
+  lat = request.args.get('lat', type = float)
+  lon = request.args.get('lon', type = float)
+  distance = request.args.get('distance', type = float, default = 0.0003)
 
-# Draw rectangle from the coordinates position
-coordinateInitial = (round(lat - distance, 6), round(lon - distance, 6))
-coordinateEnd = (round(lat + distance, 6), round(lon + distance, 6))
+  if apiKey is None:
+        abort(400, description="apiKey param required")
+  if lat is None:
+        abort(400, description="lat param required")
+  if lon is None:
+        abort(400, description="lon param required")
+  
+  threads = 20
+  radius = 100
+  allPlaces = False
+  placeTypes = []
 
+  # Calculate an area around the user using the distance
+  coordinateInitial = (round(lat - distance, 6), round(lon - distance, 6))
+  coordinateEnd = (round(lat + distance, 6), round(lon + distance, 6))
 
-print(populartimes.get(
-  apiKey, 
-  placeTypes,
-  coordinateInitial,
-  coordinateEnd,
-  threads,
-  radius,
-  allPlaces
-))
+  times = populartimes.get(
+    apiKey, 
+    placeTypes,
+    coordinateInitial,
+    coordinateEnd,
+    threads,
+    radius,
+    allPlaces
+  )
+
+  return jsonify(times)
+
